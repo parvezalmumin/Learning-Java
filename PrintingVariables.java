349d39e6bb1ac140b9e8f65b43c45d7e73d29c3e
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-09-16 12:20:31 -0700

    Printing Variables code added

---
+++ PrintingVariables.java
@@ -0,0 +1,32 @@
+ public class PrintingVariables {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am learning Java Variables.");
+       
+       String name = "Masho";
+System.out.println("Hello, " + name);
+       
+        String firstName = "Masho ";
+    String lastName = "Pramanik";
+String fullName = firstName + lastName;
+System.out.println(fullName);
+
+int x = 5;
+int y = 6;
+System.out.println(x + y); // Print the value of x + y
+
+//Multiple Variables
+int q = 5;
+int w = 6;
+int r = 50;
+System.out.println(q + w + r);
+
+int a = 51, b = 62, c = 50;
+System.out.println(a + b + c);
+
+int d, e, f;
+d = e = f = 50;
+System.out.println(d + e + f);
+
+    }
+}
