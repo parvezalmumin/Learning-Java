615c9bfdd70c6b8c4a08055dce5d755c884cfa75
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-09-16 11:42:32 -0700

    Print Text and Number code added

---
+++ Print.java
@@ -0,0 +1,9 @@
+ public class Print {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I am learning Java.");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(3 + 3);
+    }
+}
