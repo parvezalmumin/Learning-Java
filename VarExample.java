98dce992d0db8c038f008d3e1778b7cb74049ac0
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-09-16 23:45:39 -0700

    Var Example Added

---
+++ VarExample.java
@@ -0,0 +1,16 @@
+ public class VarExample {
+    public static void main(String[] args) {
+ var myNum = 5;         // int
+var myDouble = 9.98;   // double
+var myChar = 'D';      // char
+var myBoolean = true;  // boolean
+var myString = "Hello"; // String
+
+System.out.println("Integer: " + myNum);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Char: " + myChar);
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println("String: " + myString);
+}
+
+}
