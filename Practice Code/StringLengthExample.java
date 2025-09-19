598b77bb7283cb3215cf73d1b23c167fb68e0846
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-09-18 23:13:55 -0700

    String Length Practice Code Added

---
+++ Practice Code/StringLengthExample.java
@@ -0,0 +1,7 @@
+ public class StringLengthExample {
+    public static void main(String[] args) {
+        String sentence = "Java is fun!";
+        System.out.println("Sentence: " + sentence);
+        System.out.println("Length of sentence: " + sentence.length());
+    }
+}
