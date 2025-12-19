440b5bac20f4495a19dae6c8e9be5c4a2967e3a1
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-18 18:22:49 -0800

    First Commit

---
+++ src/main/java/javalearning/FirstJava.java
@@ -0,0 +1,6 @@
+package javalearning;
+public class FirstJava {
+     public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/javalearning/PrintingName.java
@@ -0,0 +1,7 @@
+package javalearning;
+
+public class PrintingName {
+      public static void main(String[] args) {
+        System.out.println("Parvez Al Mumin");
+}
+}
