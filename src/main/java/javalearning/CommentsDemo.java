6f408998206c2dc478318ead618c14e433930e2a
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-18 22:56:48 -0800

    Comments Example Practice Code Added

---
+++ src/main/java/javalearning/CommentsDemo.java
@@ -0,0 +1,17 @@
+
+package javalearning;
+
+public class CommentsDemo {
+  public static void main(String[] args){
+      /*
+ this is a
+ multi-line comment
+
+This program prints
+a welcome message
+*/
+System.out.println("Welcome");   // this is a single-line comment
+
+
+  }  
+}
