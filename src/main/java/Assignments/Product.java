38f122d6609f0f9e2f7dcf72ec8821e7ac974c02
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-18 19:36:45 -0800

    Assignment 01 Done

---
+++ src/main/java/Assignments/Product.java
@@ -0,0 +1,26 @@
+/*
+Assignment 01
+This Assignment is from Anisul Islam's Java Tutorial Playlist - Assignment 01
+
+Question 
+create a class called product
+create a main method
+print a following data in the method
+
+id: 101,
+title:Iphone15,
+price: 1895 euros,
+description: perfect product with best image quality,
+category: phone
+
+ */
+package Assignments;
+public class Product {
+    public static void main(String[] args ){
+        System.out.println("ID: 101,");
+        System.out.println("Title: Iphone 15,");
+        System.out.println("Price: 1895 Euro,");
+        System.out.println("Description: Perfect product with best image quality,");
+        System.out.println("Category: Phone");
+    }
+}
