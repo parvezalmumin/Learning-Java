0113da060b7f440ca9a9e49688f4edd33f404bf2
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-18 21:28:10 -0800

    Escape Sequence Practice Code Added

---
+++ src/main/java/javalearning/EscapeSequence.java
@@ -0,0 +1,11 @@
+package javalearning;
+public class EscapeSequence {
+    public static void main(String[] args){
+        System.out.println("Hi\nMy name is Parvez Al Mumin.\nI am learning Java.");
+        System.out.println("I am interested on Java Development.\t Or should I learn Mern Stack.");
+        System.out.println("He said, \"Hello\"");
+        System.out.println(" \'Java Programming\' ");
+        System.out.println("C:\\Program Files\\Java");
+
+    } 
+}
