cd6646bcffe9cf2a065a1d068f00e6102ce43e7e
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-18 18:49:32 -0800

    New Line Example Practice code

---
+++ src/main/java/javalearning/NewClass.java
@@ -0,0 +1,9 @@
+package javalearning;
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println("Bye World");
+        System.out.println("Allah Hafez");
+    }
+}
