52ecae55ae00b88ff99e0d08e70bb82ad5873d9f
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-19 17:05:19 -0800

    Variables and DataType code practice added

---
+++ src/main/java/javalearning/VariablesAndDataType.java
@@ -0,0 +1,24 @@
+package javalearning;
+
+public class VariablesAndDataType {
+    public static void main(String[] args){
+        byte b = 50;
+        short s = 500;
+        int i = 5000;
+        long l = 50000L;
+        float price = 10.5f;
+        double pi = 3.14159;
+        char letter = 'A';
+        boolean isJavaEasy = true;
+      
+           System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + price);
+        System.out.println("Double value: " + pi);
+        System.out.println("Char value: " + letter);
+        System.out.println("Boolean value: " + isJavaEasy);
+
+    }
+}
