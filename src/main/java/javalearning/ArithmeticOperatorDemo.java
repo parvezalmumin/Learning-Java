8cbc410edeffee263db47af37b823ffe43a3da94
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-21 21:30:10 -0800

    Arithmetic Operators Practice Code Added

---
+++ src/main/java/javalearning/ArithmeticOperatorDemo.java
@@ -0,0 +1,36 @@
+
+package javalearning;
+
+public class ArithmeticOperatorDemo {
+    public static void main(String[] args) {
+        int num1, num2, result;
+        
+        num1 = 5;
+        num2  = 2;
+        
+        //sum
+        result = num1 + num2;
+        System.out.println("Sum: " + result);
+        
+        //sub
+        result = num1 - num2;
+        System.out.println("Sub: " + result);
+        
+        
+        //Multiply
+        result = num1 * num2;
+        System.out.println("Mul: " + result);
+        
+        
+        //remainder
+        result = num1 % num2;
+        System.out.println("Remainder: " + result);
+               
+              
+        //Div
+         int num3 = 4;
+         int num4 = 2;
+         int result1 = num3/num4;
+        System.out.println("Div: " + result1);
+    }
+}
