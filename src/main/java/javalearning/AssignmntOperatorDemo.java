c81dff8401b244ed5d3fbf9e57cdac224d6a9982
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-22 17:09:29 -0800

    Assignment Operator Practice Added

---
+++ src/main/java/javalearning/AssignmntOperatorDemo.java
@@ -0,0 +1,23 @@
+package javalearning;
+public class AssignmntOperatorDemo {
+    public static void main(String[] args) {
+        int x,y;
+        x = 5;
+        y = 2;
+        
+        x += y;
+        System.out.println("x = " + x);
+        
+        x -= y;
+        System.out.println("x = " + x);
+        
+        x *= y;
+        System.out.println("x = " + x);
+        
+        x /= y;
+        System.out.println("x = " + x);
+        
+        x %= y;
+        System.out.println("x = " + x);
+    }
+}
