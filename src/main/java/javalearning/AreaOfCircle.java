fa442bd5a5e735508be995469dfaf48c76f8e972
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-25 22:38:19 -0800

    Area Of Circle practice code added

---
+++ src/main/java/javalearning/AreaOfCircle.java
@@ -0,0 +1,25 @@
+package javalearning;
+
+import  java.util.Scanner;
+
+public class AreaOfCircle { 
+    public static void main(String[] args) {
+           Scanner input = new Scanner(System.in); 
+           double pi,r,A;
+           pi = 22/7;
+           
+           System.out.println("Enter the value of radius: ");
+           r = input.nextDouble();
+           
+           System.out.println("You entered " + r);
+           System.out.println("Value of PI is 22/7");
+           
+           A = pi * (r * r);
+           System.out.println("Area of Circle is " + A);
+           
+    }
+
+   
+   
+   
+}
