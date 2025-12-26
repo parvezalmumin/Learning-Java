5efa70a9a80db57d4305746288b4d87ca417c02a
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-26 00:08:54 -0800

    Area of Rectangle Practice Code Added

---
+++ src/main/java/javalearning/AreaOfRectangle.java
@@ -0,0 +1,14 @@
+package javalearning;
+import java.util.Scanner;
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double l,w,A;
+        System.out.println("Give Length of Rectangle: ");
+        l = input.nextDouble();
+        System.out.println("Give Width Of Rectangle: ");
+        w = input.nextDouble();
+        A = l * w;
+        System.out.println("Area of Rectangle is:" + A);
+    }
+}
