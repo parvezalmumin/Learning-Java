6be579bb045c1f49fb0d21b3515ad72fb0b35414
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-28 00:43:52 -0800

    Else If Code Practice Added

---
+++ src/main/java/javalearning/ElseIf.java
@@ -0,0 +1,21 @@
+package javalearning;
+
+import java.util.Scanner;
+public class ElseIf {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give number");
+        int num = input.nextInt();
+        
+        if(num>0){
+            System.out.println("Number is Positive");
+        }
+        else if(num<0){
+            System.out.println("Number is Negative");
+        }
+        else{
+            System.out.println("number = zero");
+        }  
+    }
+}
