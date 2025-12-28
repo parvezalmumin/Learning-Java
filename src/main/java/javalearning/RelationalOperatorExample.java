898ae80a225825952fd68720b003ad4c875c44b2
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-12-27 22:02:56 -0800

    Relational Operator Example

---
+++ src/main/java/javalearning/RelationalOperatorExample.java
@@ -0,0 +1,16 @@
+
+package javalearning;
+
+public class RelationalOperatorExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a == b);   // false
+        System.out.println(a != b);   // true
+        System.out.println(a > b);    // false
+        System.out.println(a < b);    // true
+        System.out.println(a >= b);   // false
+        System.out.println(a <= b);   // true
+    }
+}
